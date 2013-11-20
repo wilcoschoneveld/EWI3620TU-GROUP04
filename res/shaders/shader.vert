d1aebf76159bed578e658f3e54ddd12e9cd01a18
bartkeulen <bart_keulen@hotmail.com>
2013-11-20 14:00:55 +0100

    Merge origin/master
    Conflicts:
    	src/Level.java
    	src/Lighting.java
    	src/Main.java
    	src/Utils.java

--- res/shaders/shader.vert
+++ res/shaders/shader.vert
@@ -1,13 +1,8 @@
-varying vec3 normal;
-varying vec3 vertex_to_light_vector;
+varying vec3 color;
  
 void main()
 {
-    gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
- 
-    normal = gl_NormalMatrix * gl_Normal;
- 
-    vec4 vertex_in_modelview_space = gl_ModelViewMatrix * gl_Vertex;
- 
-    vertex_to_light_vector = vec3(gl_LightSource[0].position - vertex_in_modelview_space);
+	color = gl_Color.rgb;	
+
+    	gl_Position = ftransform();
 }
--- src/patient04/lighting/Lighting.java
+++ src/patient04/lighting/Lighting.java
@@ -1,9 +1,9 @@
 package patient04.lighting;
 
-
 import java.nio.FloatBuffer;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL20;
+import patient04.level.Level;
 import patient04.utilities.Buffers;
 
 /**
@@ -11,29 +11,42 @@ import patient04.utilities.Buffers;
  * @author Bart
  */
 public class Lighting {
-    FloatBuffer L0position;
+    FloatBuffer
+            L0position,
+            L1position,
+            L1direction;
+    
     public static int shaderProgram1;
     
     public Lighting() {
         // Set shade model to phong
         GL11.glShadeModel(GL11.GL_SMOOTH);
         
-                //        GL11.glLightModel(GL11.GL_LIGHT_MODEL_AMBIENT, 
-//                Utils.createFloatBuffer(0.05f, 0.05f, 0.05f, 1f));
+        GL11.glLightModel(GL11.GL_LIGHT_MODEL_AMBIENT, 
+            Buffers.DARKGREY);
         
         // Enable lighting
         GL11.glEnable(GL11.GL_LIGHTING);
         
+        L0position = Buffers.createFloatBuffer(0, 15, 0, 1);
+        L1position = Buffers.createFloatBuffer(5, Level.WALL_HEIGHT, 5, 1);
+        L1direction = Buffers.createFloatBuffer(0, -1, 0, 1);
+        update();
+        
         // Set light 0 parameters
         GL11.glLight(GL11.GL_LIGHT0, GL11.GL_DIFFUSE, Buffers.WHITE);
         
-        L0position = Buffers.createFloatBuffer(0, 15, 0, 1);
-        GL11.glLight(GL11.GL_LIGHT0, GL11.GL_POSITION, L0position);        
-        
         GL11.glEnable(GL11.GL_LIGHT0);
         
+        // Set light 1 parameters
+        GL11.glLight(GL11.GL_LIGHT1, GL11.GL_DIFFUSE, Buffers.WHITE);
+        GL11.glLightf(GL11.GL_LIGHT1, GL11.GL_SPOT_CUTOFF, 80);
+        
+        GL11.glEnable(GL11.GL_LIGHT1);
+        
+        // Shaderprogram 1, floor shader
         shaderProgram1 = ShaderLoader.loadShaderPair(
-                "res/shaders/shader.vert", "res/shaders/shader.frag");
+                "res/shaders/diffuse.vert", "res/shaders/diffuse.frag");
     }
     
     public void cleanup() {
@@ -41,6 +54,8 @@ public class Lighting {
     }
     
     public void update() {
-        GL11.glLight(GL11.GL_LIGHT0, GL11.GL_POSITION, L0position);      
+        GL11.glLight(GL11.GL_LIGHT0, GL11.GL_POSITION, L0position);
+        GL11.glLight(GL11.GL_LIGHT1, GL11.GL_POSITION, L1position);
+        GL11.glLight(GL11.GL_LIGHT1, GL11.GL_SPOT_DIRECTION, L1direction);
     }
 }
