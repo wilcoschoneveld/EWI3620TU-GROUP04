2bcc46b834ee4a48e4390dce603d527689808ae6
bartkeulen <bart_keulen@hotmail.com>
2013-12-05 12:26:01 +0100

    enemy4

--- src/patient04/enemies/Path.java
+++ src/patient04/enemies/Path.java
@@ -55,47 +55,110 @@ public class Path {
     }
     
     public void calculate() {
+        boolean stop = false;
         int j = 0;
-        
-        // Calculate the nearest waypoint, return to previous waypoint when 
-        // that's the only neighbor
-        if (nextWaypoint.neighbors.size() != 1){
-            if (prevWaypoint == nextWaypoint.neighbors.get(j)) {
-                j++;
-            }
-                 
-            if (nextWaypoint.neighbors.size() > 2) {
-                for (int i = 0; i < nextWaypoint.neighbors.size(); i++) {
-                    if (prevWaypoint == nextWaypoint.neighbors.get(i)) {
-                     
-                    }
+        float change;
+        float[] changes;
+    
+        if (nextWaypoint.neighbors.size() <= 2) {
+            stop = true;
+            if (nextWaypoint.neighbors.size() == 2) {
+                if (prevWaypoint == nextWaypoint.neighbors.get(j)) {
+                    j++;
                 }
             }
+        } 
+        
+        if (stop == false) {
+            changes = calculateChanges();
+            change = (float) Math.random();
+            for (int i = 0; i < nextWaypoint.neighbors.size(); i++) {
+                    if (change <= changes[i]) {
+                        j = i;
+                        break;
+                    }
+            }
         }
+        
         prevWaypoint = nextWaypoint;
         nextWaypoint = nextWaypoint.neighbors.get(j);
         nextWaypoint.addPheromone();
     }
     
+    public float[] calculateChanges() {
+        int[] pheromones; 
+        float [] calcChange, finChange;
+        int pherTot = 0;
+        int calcTot = 0;
+        float tmpChange = 0;
+        
+        pheromones = new int[nextWaypoint.neighbors.size()];
+        for (int i = 0; i < nextWaypoint.neighbors.size(); i++) {
+            if (prevWaypoint != nextWaypoint.neighbors.get(i)) {
+                pheromones[i] = nextWaypoint.neighbors.get(i).getPheromone();
+                pherTot += pheromones[i];
+            }
+        }
+        
+        calcChange = new float[nextWaypoint.neighbors.size()];
+        for (int i = 0; i < nextWaypoint.neighbors.size(); i++) {
+            if (prevWaypoint != nextWaypoint.neighbors.get(i)) { 
+                calcChange[i] = pherTot / pheromones[i];
+                calcTot += calcChange[i];
+            }
+        }
+        
+        finChange = new float[nextWaypoint.neighbors.size()];
+        for (int i = 0; i < nextWaypoint.neighbors.size(); i++) {
+            if (prevWaypoint != nextWaypoint.neighbors.get(i)) { 
+                tmpChange += calcChange[i];
+                finChange[i] = tmpChange / calcTot;
+            }
+        }
+        
+        return finChange;
+    }
+    
     public void testPath() {
-        Waypoint n0 = new Waypoint(new Vector(10f, 0, 4.5f));
-        Waypoint n1 = new Waypoint(new Vector(4.5f, 0, 4.5f));
-        Waypoint n2 = new Waypoint(new Vector(4.5f, 0, 20f));
-        Waypoint n3 = new Waypoint(new Vector(10f, 0, 23f));
-        Waypoint n4 = new Waypoint(new Vector(4.5f, 0, 23f));
-        Waypoint n5 = new Waypoint(new Vector(10f, 0, 23f));
-               
+        Waypoint n0 = new Waypoint(new Vector(4.5f, 0, 4.5f));
+        Waypoint n1 = new Waypoint(new Vector(4.5f, 0, 20f));
+        Waypoint n2 = new Waypoint(new Vector(4.5f, 0, 23f));
+        Waypoint n3 = new Waypoint(new Vector(6f, 0, 23f));
+        Waypoint n4 = new Waypoint(new Vector(10f, 0, 23f));
+        Waypoint n5 = new Waypoint(new Vector(10f, 0, 20f));
+        Waypoint n6 = new Waypoint(new Vector(10f, 0, 4.5f));
+        Waypoint n7 = new Waypoint(new Vector(6f, 0, 20f));
+        Waypoint n8 = new Waypoint(new Vector(6f, 0, 7f));
+        
         this.addWaypoint(n0);
         this.addWaypoint(n1);
         this.addWaypoint(n2);
-        this.addWaypoint(n4);
         this.addWaypoint(n3);
+        this.addWaypoint(n4);
         this.addWaypoint(n5);
+        this.addWaypoint(n6);
         
-        Waypoint.link(n5, n0);
-        Waypoint.link(n2, n5);
-        Waypoint.link(n2, n3);
-        Waypoint.link(n4, n5);
+        this.addSingleWaypoint(n7);
+        this.addSingleWaypoint(n8);
         
+        Waypoint.link(n1, n7);
+        Waypoint.link(n3, n7);
+        Waypoint.link(n5, n7);
+        Waypoint.link(n0, n6);
+        Waypoint.link(n0, n8);
+        Waypoint.link(n6, n8);
+        
+        
+//        Waypoint n0 = new Waypoint(new Vector(4.5f, 0, 4.5f));
+//        Waypoint n1 = new Waypoint(new Vector(10f, 0, 4.5f));
+//        Waypoint n2 = new Waypoint(new Vector(10f, 0, 10f));
+//        Waypoint n3 = new Waypoint(new Vector(4.5f, 0, 7f));
+//        
+//        this.addWaypoint(n0);
+//        this.addWaypoint(n1);
+//        this.addWaypoint(n2);
+//        this.addWaypoint(n3);
+//        
+//        Waypoint.link(n3, n0);
     }
 }
