e10e04d0c0174e3bca918bc37cb7e9a242bdaf0c
Wilco <Wilco@Wilco-Laptop.lan>
2013-12-18 20:36:38 +0100

    added medicine levels

--- src/patient04/level/Player.java
+++ src/patient04/level/Player.java
@@ -6,7 +6,6 @@ import patient04.physics.Entity;
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
 import patient04.math.Matrix;
-import patient04.physics.AABB;
 import patient04.utilities.Input;
 
 /**
@@ -17,16 +16,24 @@ public class Player extends Entity implements Input.Listener {
     // Width and height of the player, used as bounding box
     public static final float WIDTH = 0.6f;
     public static final float HEIGHT = 1.8f;
-
-    // Determines where the camera is located
-    public static final float EYEHEIGHT = 1.7f;
+    public static final float EYE_HEIGHT = 1.65f;
     
     // Movement acceleration
-    public static final float ACCEL_WALKING = 1f;
-    public static final float ACCEL_RUNNING = 2f;
+    public static final float ACCEL_WALKING = 0.7f;
+    public static final float ACCEL_RUNNING = 1.5f;
     public static final float ACCEL_AIR = 0.1f;
     public static final float ACCEL_JUMP = 0.5f;
+    
+    // Patient treatment
+    public static final float MEDICINE_USE_RATE = 0.05f; // per second
+    public static final float MEDICINE_CAN_RUN = 0.5f;
+    public static final float MEDICINE_CAN_MOVE = 0.01f;
 
+    // Player variables
+    private float viewBobbing = 0;
+    private float medicineLevel = 1;
+    private float viewHeight = EYE_HEIGHT;
+    
     /** Constructs a new player.
      * 
      * @param level 
@@ -41,24 +48,34 @@ public class Player extends Entity implements Input.Listener {
      */
     @Override
     public void update(float dt) {
+        // Use some medicine
+        medicineLevel -= MEDICINE_USE_RATE * dt;
+        
         // Define a new movement vector
         Vector moveInput = new Vector();
         
         // Handle input
-        if(Keyboard.isKeyDown(Keyboard.KEY_W)) moveInput.add(0, 0, -1);
-        if(Keyboard.isKeyDown(Keyboard.KEY_S)) moveInput.add(0, 0, 1);
-        if(Keyboard.isKeyDown(Keyboard.KEY_A)) moveInput.add(-1, 0, 0);
-        if(Keyboard.isKeyDown(Keyboard.KEY_D)) moveInput.add(1, 0, 0);
-
-        // If input is given
+        if (medicineLevel > MEDICINE_CAN_MOVE) {
+            if(Keyboard.isKeyDown(Keyboard.KEY_W)) moveInput.add(0, 0, -1);
+            if(Keyboard.isKeyDown(Keyboard.KEY_S)) moveInput.add(0, 0, 1);
+            if(Keyboard.isKeyDown(Keyboard.KEY_A)) moveInput.add(-1, 0, 0);
+            if(Keyboard.isKeyDown(Keyboard.KEY_D)) moveInput.add(1, 0, 0);
+        }
+        
+        // If movement input is given
         if(moveInput.length() > 0) {
             // Rotate inputForce according to viewing direction
             moveInput.rotate(rotation.y, 0, 1, 0);
             
             // Determine movement speed
             float speed = ACCEL_WALKING * dt;
-            if(Keyboard.isKeyDown(Keyboard.KEY_LSHIFT))
+            
+            // If trying to run and is allowed
+            if(Keyboard.isKeyDown(Keyboard.KEY_LSHIFT)
+                                    && medicineLevel > MEDICINE_CAN_RUN)
                 speed = ACCEL_RUNNING * dt;
+            
+            // If not on the ground
             if(onGround == false)
                 speed = ACCEL_AIR * dt;
             
@@ -78,55 +95,43 @@ public class Player extends Entity implements Input.Listener {
      * @return 
      */
     public Matrix getFirstPersonView() {
-        
-        viewbobbing *= 0.9f;
-        
-        if(onGround)
-            viewbobbing += 0.1f;
-        
+        // Create new view matrix
         Matrix matrix = new Matrix();
         
-        Vector leanInput = new Vector();
-        if(Keyboard.isKeyDown(Keyboard.KEY_Q)) leanInput.add(-1, 0, 0);
-        if(Keyboard.isKeyDown(Keyboard.KEY_E)) leanInput.add(1, 0, 0);
-        
-        if(leanInput.x != 0) {
-            leanInput.rotate(rotation.y, 0, 1, 0);
+        // Drop to ground if player has no more medicine
+        if (medicineLevel < 0)
+            viewHeight = Math.max(viewHeight - 0.03f, 0.25f);
             
-            // Create head aabb
-            AABB aabb2 = aabb.copy();
-            aabb2.min.add(0, 1.6f,0);
-            aabb2.pos.add(leanInput);
-            
-            // Check if collision free
-            boolean isFree = level.getCollisionBoxes(aabb2).isEmpty();
-        }
+        // Calculate view bobbing
+        viewBobbing = viewBobbing * 0.9f + (onGround ? 0.1f : 0);
         
         matrix.translate(
-                (float)  Math.cos(distanceMoved * 3) * 0.05f * viewbobbing,
-                (float)  Math.cos(distanceMoved * 6) * 0.05f * viewbobbing, 0);
+                (float)  Math.cos(distanceMoved * 3) * 0.05f * viewBobbing,
+                (float)  Math.cos(distanceMoved * 6) * 0.05f * viewBobbing, 0);
         matrix.rotate(
-                (float) -Math.cos(distanceMoved * 3) * 0.05f * viewbobbing,
+                (float) -Math.cos(distanceMoved * 3) * 0.05f * viewBobbing,
                 0, 0, 1);
         
+        matrix.rotate((EYE_HEIGHT - viewHeight) * 30, 0, 0, 1);
         matrix.rotate(-rotation.x, 1, 0, 0);
         matrix.rotate(-rotation.y, 0, 1, 0);
         matrix.translate(
                 -position.x,
-                -position.y - EYEHEIGHT,
+                -position.y - viewHeight,
                 -position.z);
         
         return matrix;
     }
     
-    private float viewbobbing = 0;
+    
 
     @Override
     public boolean handleMouseEvent() {
+        // Poll for mouse movement
         int dx = Mouse.getEventDX(), dy = Mouse.getEventDY();
         
-        if (dx != 0 || dy != 0) {
-            // Update the camera orientation from mouse movement
+        // Update the camera orientation from mouse movement
+        if ((dx != 0 || dy != 0) && medicineLevel > MEDICINE_CAN_MOVE) {
             rotation.y -= 0.1 * Mouse.getEventDX();
             rotation.x += 0.1 * Mouse.getEventDY();
             if (rotation.x > 90) rotation.x = 90;
@@ -140,9 +145,16 @@ public class Player extends Entity implements Input.Listener {
 
     @Override
     public boolean handleKeyboardEvent() {
-        // Handle jump key
+        // Handle jump key (TODO: REMOVE)
         if (Input.keyboardKey(Keyboard.KEY_SPACE, true) && onGround) {
             acceleration.add(0, ACCEL_JUMP, 0);
+            
+            return Input.HANDLED;
+        }
+        
+        // Handle self injection (TODO: REMOVE)
+        if (Input.keyboardKey(Keyboard.KEY_G, true)) {
+            medicineLevel = 1;
             
             return Input.HANDLED;
         }
