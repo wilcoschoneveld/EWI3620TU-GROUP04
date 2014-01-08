26bf33ed85a7c633604965afdf88e430f4d7445f
bartkeulen <bart_keulen@hotmail.com>
2014-01-08 16:38:28 +0100

    Merge origin/master into BartSpotted
    Conflicts:
    	src/patient04/level/Level.java
    	src/patient04/states/Game.java

--- src/patient04/Main.java
+++ src/patient04/Main.java
@@ -12,9 +12,9 @@ import patient04.utilities.Utils;
 
 public final class Main {
     // Window dimensions
-    public static final int desiredWidth = 800;
-    public static final int desiredHeight = 500;
-    public static final boolean fullscreen = false;
+    public static final int desiredWidth = 1280;
+    public static final int desiredHeight = 720;
+    public static final boolean fullscreen = true;
     public static final boolean vsyncEnabled = true;
     
     // Possible states
