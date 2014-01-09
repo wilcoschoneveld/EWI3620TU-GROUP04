6b796903be1745c9bff81ad0cb0140976b176fab
Wilco <Wilco@Wilco-Laptop.lan>
2014-01-09 23:37:10 +0100

    added comments

--- src/patient04/resources/Database.java
+++ src/patient04/resources/Database.java
@@ -5,36 +5,44 @@ import java.util.ArrayList;
 
 /**
  *
- * @author Wilco
+ * @author Kaj Dreef
  */
 public class Database {
     
     public Database() {
         // Create database if it does not exist        
         try {
+            // Make sure static JDBC class is loaded
             Class.forName("org.sqlite.JDBC");
             
+            // Connect to database
             Connection c = DriverManager.getConnection("jdbc:sqlite:scores.db");
             
+            // Create a new statement
             Statement s = c.createStatement();
             
+            // Execute an update statement
             s.executeUpdate("CREATE TABLE IF NOT EXISTS "
                           + "highscore (playerName STRING, time FLOAT);");
             
+            // Close connection
             s.close(); c.close();
         } catch(Exception e) { e.printStackTrace(); }
     }
     
     public void addTime(String name, float time) {     
         try {
+            // Connect to database
             Connection c = DriverManager.getConnection("jdbc:sqlite:scores.db");
             
+            // Prepare and executre an insert statement
             PreparedStatement s = c.prepareStatement(
                     "INSERT INTO highscore (playerName, time) VALUES (?, ?);");
             s.setString(1, name);
             s.setFloat(2, time);
             s.executeUpdate();
             
+            // Close connection
             s.close(); c.close();
         } catch(SQLException e) { e.printStackTrace(); }
     }
@@ -43,16 +51,21 @@ public class Database {
         ArrayList<String> times = new ArrayList<>();
         
         try {
+            // Connect to database
             Connection c = DriverManager.getConnection("jdbc:sqlite:scores.db");
             
+            // Create a new statement
             Statement s = c.createStatement();
             
+            // Execute a query
             ResultSet r = s.executeQuery(
                         "SELECT * FROM highscore ORDER BY time ASC LIMIT 5;");
             
+            // Add results to arraylist
             while (r.next())
                 times.add(r.getString("playerName")+ ": " + r.getFloat("time"));
             
+            // Close connection
             r.close(); s.close(); c.close();
         } catch(Exception e) { e.printStackTrace(); }
         
